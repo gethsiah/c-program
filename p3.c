ac4387857215a624a2c6a1ea6202c6259c5851dd
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-01-24 16:31:38 +0530

    Update p3.c

--- p3.c
+++ p3.c
@@ -5,10 +5,10 @@ char ch;
 printf("Input a character\n");
 scanf("%c", &ch);
 if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
-if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch== 'u' || ch=='U')
-printf("%c is a vowel.\n", ch);
+if (ch=='l'||ch=='L'||ch=='a'||ch=='A'||ch=='c'||ch=='C'||ch=='h'||ch=='H'||ch=='u'||ch=='U')
+printf("%c is a vowel\n", ch);
 else
-printf("%c is a consonant.\n", ch);
+printf("%c is a consonant\n", ch);
 }
 else
 printf("%c is neither a vowel nor a consonant.\n", ch);
