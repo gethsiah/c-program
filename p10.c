3df41f2808882400a954489f06b9d6daed0dc6f0
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-02 16:53:15 +0530

    p10.c

---
+++ p10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int no,n,count;
+printf("enter the digit\n");
+scanf("%d",&n);
+for(i=0;i<=count;i++)
+count=n+no;
+printf("the number of digits are%d\n",count,no);
+getch();
+}
