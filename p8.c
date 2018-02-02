51448fa68ca39f860e4b1150bed63e6ea64a05bd
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-02 16:36:04 +0530

    p8.c

---
+++ p8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,no,sum=0;
+printf("\nenter an integer no");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+sum=sum+n;
+printf("sum of 1st %d natural numbers=%d\n,num,sum);
+getch();
+}
