3cc0fa27209221e0784ae575a54e496a1b052d68
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-02 16:59:37 +0530

    p11.c

---
+++ p11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n,fact=a,i;
+printf("enter the value\n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+fact=fact*i;
+printf("factorial of no is\n",fact);
+getch();
+}
