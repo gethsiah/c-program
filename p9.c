39499ddcb69e7cf79c7acf40c5497f8373f69c9a
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-02 16:44:25 +0530

    p9.c

---
+++ p9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,k,no,sum;
+printF("enter the integer\n");
+scanf("%d",&no);
+for(i=0;i<=no;i++)
+sum=sum+no;
+printf("sum of k inetgers%d\n",n,k);
+getch();
+}
