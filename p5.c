96cbc0d86baf10d12691b8373530c02bbbdc2b2b
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-02 15:55:54 +0530

    p5.c

---
+++ p5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the number a\n");
+scanf("%d",&a);
+printf("enter the number b\n");
+scanf("%d",&b);
+printf("enter the number c\n");
+scanf("%d",&c);
+if(a>b&&a>c)
+{
+printf("enter the number a is greater%d\n");
+}
+if else(b<c)
+{
+printf("the greatest number is b%d\n");
+}
+else
+{
+printf("the greatest number is c%d\n");
+}
+getch();
+}
+
