09658bba70753bac5e9d1a23ad2dd70381cf720e
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-05 16:40:30 +0530

    p14.c

---
+++ p14.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,n,f=0;
+printf("enter a positive num\n");
+scanf("%d",&n);
+for(i=1;i<=1;i++)
+{
+if(n%i==0)
+}
+f=1;
+break;
+if(f==0)
+printf("%d is a prime num\n",n);
+else
+printf("%d is not a prime num\n");
+return 0;
+}
