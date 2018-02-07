bf507995073bdac55d82a91ad98c8b28ff891286
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-07 16:16:20 +0530

    p19.c

---
+++ p19.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int l,h,i,t1,t2,r,n=0,res= 0;
+printf("Enter two numbs of intervals\n");
+scanf("%d %d",&l,&h);
+printf("Armstrong numbers between %d an %d are",l,h);
+for(i=l+1;i<h;++i)
+{
+t2=i;
+t1= i;
+while (t1!= 0)
+t1/=10;
+++n;
+while (t2!= 0)
+r=t2% 10;
+res+=p(r,n);
+t2/=10;
+}
+if(res==i) 
+printf("%d ", i);
+n = 0;
+res=0;
+}
+return 0;
+}
