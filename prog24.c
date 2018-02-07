549d745146f7b653c7e657fe8eb838190c98a028
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-07 16:58:53 +0530

    prog24.c

---
+++ prog24.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+void main()
+{
+void search,int l, int h)
+{
+if(l>h)
+return;
+if(l==h)
+printf("The req element is %d\n",a[l]);
+return;
+int mid=(l+h)/2;
+if(mid%2==0)
+search(a,mid+2;h);
+else
+search(arr, low, mid);
+if (a[mid]==a[mid-1])
+search(a,mid-2,high);
+else
+search(a,l,mid-1);
+return 0;
+}
