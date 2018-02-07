a7fd2d51aca64cbeb16b12f0efe08c1dd70736f6
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-07 15:42:47 +0530

    p16.c

---
+++ p16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int 2,2,i;
+printf("enter the fir num\n");
+scanf("%d",&1);
+printf("enter the sec num\n);
+scanf("%d",&2);
+for(i=2;i<=2;i++)
+printf("the given numbers between two ranges are\n"2,2);
+return 0;
+}
