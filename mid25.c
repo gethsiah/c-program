5c2ea3f86843e1c5957916f48acc5243e175d42d
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-09 15:48:18 +0530

    mid25.c

---
+++ mid25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+int ismiddle3(int a[ ], int l) 
+{
+int sum=0;
+for(int i=0;i<l;i++){
+sum+=a[i];
+if(sum<10)
+{
+printf("Sum of all the elements are less than 10");
+elseif(l%2==0)
+printf("the middle element is\n");
+else
+{
+for(int i=0;i<len;i++)
+int ismiddle3(int a[ ], int l);
+} 
+getch();
+}
