0ad89326f0404431eacc7870f8c568fd648ea689
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-09 16:36:16 +0530

    index28.c

---
+++ index28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX_SIZE 2000
+int main()
+{
+int arr[MAX_SIZE];
+int n, N;
+printf("Enter the size of array\n%d",N);
+printf("%dEnter elements in the array\n%d", N,arr[n]);
+for(n=0; n<N; n++)
+printf("\nElements in array are");
+for(n=0; n<N; n++)
+{
+printf("%d, ", arr[n]);
+}
+return 0;
+}
