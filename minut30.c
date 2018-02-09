268fa9f12d30368a3f4358d2d024eace63bcef2b
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-09 16:49:48 +0530

    minut30.c

---
+++ minut30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int hr, min, total_min,t1,t2;
+printf("\nEnter the Time in Minutes two times%d%d\n"t1,t2);
+printf("\nhours\t");
+scanf("%d", &hours);
+printf("\nMinutes:\t");
+scanf("%d", &minutes);
+total_min=t1-t2;
+total_min= (hrs * 60) + min;
+printf("\nTotal Time in Minutes\t%d\n", total_min);
+return 0;
+}
