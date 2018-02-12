2423a83d11c853abb535465974a1e2f327408945
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-12 16:10:25 +0530

    swap37.c

---
+++ swap37.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the value of a\n");
+scanf("%d",&a);
+printf("enter the value of b\n");
+scanf("%d",&b);
+swap(a,b);
+{
+void swap(a,b)
+{
+int temp;
+temp=a;
+a=b;
+b=temp;
+}
+printf("before swapping%d\n,swap(a,b));
+printf("after swapping%d\n.swap(a,b));
+}
+getch();
+}
