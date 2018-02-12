f6241f0b94f931ff50fb5b359fa6a2a650dcc234
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-12 16:33:38 +0530

    line34.c

---
+++ line34.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+char s[50];
+int i, line;
+printf("enter the string\n");
+scanf("%s", s);
+for(i=0 ; s[0]!="\0" ; i++
+{
+if (b[i]='    ')
+{
+line++;
+}
+}
+printf("Number of spaces is %d", line);
+return 0;
+}
