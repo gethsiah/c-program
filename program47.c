d452f5aa6016b3fa16dbdbd7ef591caba74793b8
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-23 16:23:30 +0530

    program47.c

---
+++ program47.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char s1,s2;
+printf("enter the str 1\n%d",s1);
+printf("enter the str 2\n%d",s2);
+if(s1>s2)
+
+if(s2>s1)
+
+printf("the gretest of str is\n%d",s1,s2);
+return 0;
+}
