6dcaabd7aba2e9a87fba5a331db0a9ace0c1608f
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-23 16:00:54 +0530

    program45.c

---
+++ program45.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+long long m;
+int c= 0;
+printf("Enter an integer\n ");
+scanf("%15d", &m);
+while(n != 0)
+{
+// n = n/10
+n /= 10;
+++c;
+}    
+printf("Number of digits\n%d", c);
+}
