f3420b5a44dc863c7c87588fbac86dcfb82da7b8
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-02-28 15:54:50 +0530

    program56.c

---
+++ program56.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+char a;
+printf("Enter the character\n");
+scanf("%a",&a);
+if((a>='a' && a<='z')||(a>='A' && a<='Z'))
+if((a>='0'&&a<='n')||(a>='0'&&a<='N')
+printf("%c is an alphabet or number",a);
+else
+printf("%c is not an alphabet or number",c);
+return 0;
+}
