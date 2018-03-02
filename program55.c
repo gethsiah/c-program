4f09d4c0576544f56237565121ff0e7a8dd98c0b
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-02 15:58:14 +0530

    program55.c

---
+++ program55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,m,e;
+printf("enter the no\n");
+scanf("%d",&n);
+printf("enter the no2\n");
+scanf("%d",&m);
+e=n*m;
+{
+printf("the given no is even%d\n",e);
+else
+printf("the given no is not even%d\n",e);
+}
+return 0;
+}
