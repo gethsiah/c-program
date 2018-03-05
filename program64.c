9858067aba9ad047145511c6e6ce2bcfbeb70a48
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-05 16:25:53 +0530

    program64.c

---
+++ program64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter a value\n");
+scanf("%d",&a);
+printf("enter b value\n");
+scanf("%d",&b);
+sum=a+b;
+printf("the sum of two values are%d\n",sum);
+printf("the sum of two values ans is odd%d",sum);
+else
+{
+printf("the sum of teo values ans is even%d",sum);
+}
+return 0;
+}
