e0c8b1170559f7236028d6c4c34064047018b1a7
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-05 16:36:57 +0530

    program69.c

---
+++ program69.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,diff;
+printf("enter a value\n%d",a);
+printf("enter b value\n%d",b);
+diff=a-b;
+printf("the diff of a and b is%d\n",diff);
+{
+printf("a and b values are even");
+}
+else
+printf("a and b values are odd");
+
+return 0;
+}
