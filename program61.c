ed02b9afe71607a718ebc6e3c53b11bbe4955be0
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-05 16:51:55 +0530

    program61.c

---
+++ program61.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[20],str1[10];
+char laptop[10];
+char lap[10];
+int n;
+printf("enter the str\n");
+scanf("%s",&str);
+printf("enter the str frst char%s\n",lap);
+return 0;
+}
