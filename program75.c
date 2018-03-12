c878d93444ed2c2fe4025765d05cc0bc78a76088
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-12 16:40:51 +0530

    program75.c

---
+++ program75.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+char*strmid(char *texte, int d, int longr)
+{ char *result = " ";
+char *temp = " ";
+int nbr,n;
+nbr=d+longr - 1;
+strncat(temp,texte,nbr);
+strrev(temp);
+strncat(result,temp,longr+1);
+strrev(result);
+return result;
+printf("enter the element name");
+scanf("%d",&n);
+(if=(mid=*))
+printf("the mid element can change after %d",n);
+
+}
