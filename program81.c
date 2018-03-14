f97f1b79fd387d980510e8757c6c9f8e258be6ca
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-14 15:55:24 +0530

    program81.c

---
+++ program81.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("ninjas bw kabalis clan\n");
+scanf("%d",&a);
+printf("his opponents caln\n");
+scanf("%d',&b);
+printf("10%d,12%d");
+printf("10%d,14%d");
+return 0;
+}
