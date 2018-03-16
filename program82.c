7c809acbf0d9f96b21cd89fbe140fac5482ef043
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-16 16:47:03 +0530

    program82.c

---
+++ program82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int l, b, area;
+printf("\nEnter the Length of farm\n");
+scanf("%d", &l);
+printf("\nEnter the Breadth of farm\n");
+scanf("%d", &b);
+area = l * b;
+printf("\nArea of farm is %d\n", area);
+return (0);
+}
