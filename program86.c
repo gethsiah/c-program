4c8708a0578481a11e2b506b949b3e63063b719c
gethsiah <30549406+gethsiah@users.noreply.github.com>
2018-03-16 15:49:25 +0530

    program86.c

---
+++ program86.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char s[5],s1,s2,T;
+printf("enter the string");
+scanf("%s",&s[5]);
+s1=1 <= T <= 100
+s2=1<= |S| <= 100
+printf("isogram or not",s1,s2);
+return 0;
+}
